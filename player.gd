5b45761303100a7389374a49f61de3985425f74d
Minju Sung <sung.minj@northeastern.edu>
2026-05-06 20:43:59 -0400

    no gravity for flat map moving

--- player.gd
+++ player.gd
@@ -7,18 +7,19 @@ const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	'''if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+'''
+	# Handle jump. -don't need jump bc librarian is just moving around
+	'''if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	'''
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_vector("ui_left", "ui_right","ui_up", "ui_down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
